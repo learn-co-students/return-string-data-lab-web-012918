2b749ac67b7c367467248cc77a0c9e42446bb8cc
github username <github email address>
2018-03-24 15:56:23 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   get 'home', to: 'static#home'
   resources :orders
   resources :invoices
+  resources :products
+
+  get '/products/:id/description', to: 'products#inventory'
+  get '/products/:id/inventory', to: 'products#description'
 end
