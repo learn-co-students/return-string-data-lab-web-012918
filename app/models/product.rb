a396ca4158d02188b0920158269443e719c44696
gilsunshine <gilsunshine@gmail.com>
2018-03-26 08:35:22 -0400

    Done.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,15 @@
 class Product < ActiveRecord::Base
   has_many :ordered_products
   has_many :orders, :through => :ordered_products
+
+  attr_accessor :inventory, :description
+
+  # def inventory=(param)
+  #   return param
+  # end
+  #
+  # def description=(param)
+  #   return param
+  # end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'home', to: 'static#home'
+  get 'inventory', to: 'products#inventory'
   resources :orders
   resources :invoices
+  resources :products
 end
